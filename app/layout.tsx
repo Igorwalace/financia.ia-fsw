22edbb99010291633be559968b78d64497f18200
igoor <igorwallacy3@gmail.com>
2024-11-07 19:26:53 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Financia IA",
+  description: "Controle suas finanças de forma simples e eficiente.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`dark antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
