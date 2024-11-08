ce77fb148cfd852261504a19a059c6a988d6fa44
igoor <igorwallacy3@gmail.com>
2024-11-08 19:58:21 -0300

    feat: add middleware

---
+++ app/services/get-url.tsx
@@ -0,0 +1,6 @@
+export function getUrl(path?: string) {
+    const baseUrl = process.env.PATHNAME || '';
+    const normalizedPath =
+        path && !path.startsWith('/') ? `/${path}` : path || '';
+    return `${baseUrl}${normalizedPath}`;
+}
---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getUrl } from "./app/services/get-url";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get(process.env.PATHNAME_COOKIES || "");
+  const pathname = request.nextUrl.pathname;
+
+  if (pathname === "/" && token) {
+    return NextResponse.redirect(new URL(getUrl("/app/dashboard")));
+  }
+
+  if (pathname.includes("/app") && !token) {
+    return NextResponse.redirect(new URL(getUrl("/")));
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
