83bb236481c0bcbe0aefc74e5f22bd0590492c76
igoor <igorwallacy3@gmail.com>
2024-11-08 00:15:41 -0300

    feat: authJs

--- app/login/button-login.tsx
+++ app/login/button-login.tsx
@@ -1,10 +1,13 @@
+'use client'
+
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "../_components/ui/button";
+import { signIn } from "next-auth/react";
 
 const ButtonSignIn = () => {
     return (
         <>
-            <Button variant='outline' className='w-full text-sm font-semibold' ><FcGoogle className='' /> Entrar com o Google</Button>
+            <Button onClick={() => signIn("google")} variant='outline' className='w-full text-sm font-semibold' ><FcGoogle className='' /> Entrar com o Google</Button>
         </>
     );
 }
