cfca9311ca3e46c71cc932d140e4b57cca27636b
igoor <igorwallacy3@gmail.com>
2024-11-09 00:12:22 -0300

    feat: add page dashboard

--- app/app/dashboard/page.tsx
+++ app/app/dashboard/page.tsx
@@ -1,9 +1,30 @@
+import { Button } from "@/app/_components/ui/button";
 import Header from "../header";
+import SelectMonth from "./select";
+import Image from "next/image";
 
 const Dashboard = () => {
     return (
         <>
             <Header />
+            <div className="py-4 px-6" >
+                <div className='flex justify-between items-center' >
+                    <h1 className='text-2xl font-bold' >Dashboard</h1>
+                    <div className="flex gap-3 justify-between items-center" >
+                        <Button variant='ghost' className="text-white font-semibold" >
+                            Relatório IA
+                            <Image
+                                src="/report.svg"
+                                width={15}
+                                height={17}
+                                alt="Finance AI"
+                                className=""
+                            />
+                        </Button>
+                        <SelectMonth />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
---
+++ app/app/dashboard/select.tsx
@@ -0,0 +1,31 @@
+import { Select, SelectContent, SelectTrigger, SelectValue, SelectItem } from "@/app/_components/ui/select";
+
+
+const SelectMonth = () => {
+
+    return (
+        <>
+            <Select>
+                <SelectTrigger className="w-[125px] rounded-full">
+                    <SelectValue placeholder='Mês' />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="Janeiro">Janeiro</SelectItem>
+                    <SelectItem value="Fevereiro">Fevereiro</SelectItem>
+                    <SelectItem value="Março">Março</SelectItem>
+                    <SelectItem value="Abril">Abril</SelectItem>
+                    <SelectItem value="Maio">Maio</SelectItem>
+                    <SelectItem value="Junho">Junho</SelectItem>
+                    <SelectItem value="Julho">Julho</SelectItem>
+                    <SelectItem value="Agosto">Agosto</SelectItem>
+                    <SelectItem value="Setembro">Setembro</SelectItem>
+                    <SelectItem value="Outubro">Agosto</SelectItem>
+                    <SelectItem value="Novembro">Novembro</SelectItem>
+                    <SelectItem value="Dezembro">Dezembro</SelectItem>
+                </SelectContent>
+            </Select>
+        </>
+    );
+}
+
+export default SelectMonth;
